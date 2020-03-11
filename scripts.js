1c691420964b0aaad2062fdb38f16c82bebb5fa7
Chandler Mayo <chandler@chandlermayo.com>
2020-03-11 13:47:08 -0400

    Added get request to geo reverse endpoint.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+var geocodio_API_KEY = "b67b8419e9b6e105e6659e191e5e16104eee640"; // Get your API key https://dash.geocod.io/
+
+function makeRequest(apiURL){
+    var Httpreq = new XMLHttpRequest();
+    Httpreq.open("GET",apiURL,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
 function syntaxHighlight(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
@@ -17,5 +26,14 @@ function syntaxHighlight(json) {
     });
 }
 
-var obj = {a:1, 'b':'foo', c:[false,'false',null, 'null', {d:{e:1.3e5,f:'1.3e5'}}]};
-document.body.appendChild(document.createElement('pre')).innerHTML = syntaxHighlight(JSON.stringify(obj, undefined, 4));
+if(navigator.geolocation) { // Get current position and send to Geocod.io reverse endpoint.
+    navigator.geolocation.getCurrentPosition(function(position) {
+        // Geocode.io API URL
+        //https://api.geocod.io/v1.4/reverse?q=X,Y&api_key=YOUR_API_KEY
+        var requestResponse = JSON.parse(makeRequest("https://api.geocod.io/v1.4/reverse?q="+position.coords.latitude+","+position.coords.longitude+"&api_key="+geocodio_API_KEY));
+        document.body.appendChild(document.createElement('pre')).innerHTML = syntaxHighlight(JSON.stringify(requestResponse, undefined, 4));
+    });
+} else {
+    alert("Sorry, your browser does not support HTML5 geolocation.");
+}
+
